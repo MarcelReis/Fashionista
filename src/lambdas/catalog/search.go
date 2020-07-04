fbd5756a85045a4755cd5275927016e71481f954
Marcelo Reis <oi@marcelreis.dev>
2020-07-04 17:42:42 -0300

    git push

--- src/lambdas/catalog/search.go
+++ src/lambdas/catalog/search.go
@@ -1,6 +1,12 @@
 package main
 
-import "strings"
+import (
+	"strings"
+	"unicode"
+
+	"golang.org/x/text/transform"
+	"golang.org/x/text/unicode/norm"
+)
 
 // SearchProduct ...
 func SearchProduct(search string, products []Product) []Product {
@@ -20,10 +26,30 @@ func filter(products []Product, shouldAdd func(Product, string) bool, search str
 }
 
 func shouldAdd(product Product, search string) bool {
-	productName := strings.ToLower(product.Name)
-
-	if strings.Contains(productName, search) {
+	if findByName(product.Name, search) || findByID(product.Style, search) {
 		return true
 	}
 	return false
+}
+
+func findByName(name string, search string) bool {
+	name = strings.ToLower(name)
+	name = removeDiacrits(name)
+	return strings.Contains(name, search)
+}
+
+func isMn(r rune) bool {
+	return unicode.Is(unicode.Mn, r) // Mn: nonspacing marks
+}
+
+func removeDiacrits(value string) string {
+	t := transform.Chain(norm.NFD, transform.RemoveFunc(isMn), norm.NFC)
+	result, _, _ := transform.String(t, value)
+	return result
+}
+
+func findByID(id string, search string) bool {
+	stylesParams := strings.Split(search, "_")
+
+	return id == stylesParams[0]
 }
